ee7eda22aa58530724e29fade9319a0debcda312
Ezer Belhassen <m.ezer.belhassen@gmail.com>
2019-01-02 22:59:17 +0100

    Updates

---
+++ precache-manifest.4a7fd51eb9669ef247e8a0161472b940.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = [
+  {
+    "revision": "ecdd509cadbf1ea78b8d2e31ec52328c",
+    "url": "/ezerbelhassen/inspirity-react/static/media/Roboto-Bold.ecdd509c.eot"
+  },
+  {
+    "revision": "14e371796624f8bcbb41",
+    "url": "/ezerbelhassen/inspirity-react/static/css/main.5804050e.chunk.css"
+  },
+  {
+    "revision": "e31fcf1885e371e19f5786c2bdfeae1b",
+    "url": "/ezerbelhassen/inspirity-react/static/media/Roboto-Bold.e31fcf18.ttf"
+  },
+  {
+    "revision": "330df886e97df17de249",
+    "url": "/ezerbelhassen/inspirity-react/static/js/1.330df886.chunk.js"
+  },
+  {
+    "revision": "1634a1a34cc30fccdbfc",
+    "url": "/ezerbelhassen/inspirity-react/static/js/runtime~main.1634a1a3.js"
+  },
+  {
+    "revision": "beaab26664137dc78edd6a735b31b40d",
+    "url": "/ezerbelhassen/inspirity-react/static/media/moon.beaab266.otf"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/ezerbelhassen/inspirity-react/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/ezerbelhassen/inspirity-react/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/ezerbelhassen/inspirity-react/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/ezerbelhassen/inspirity-react/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/ezerbelhassen/inspirity-react/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/ezerbelhassen/inspirity-react/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/ezerbelhassen/inspirity-react/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "dfe56a876d0282555d1e2458e278060f",
+    "url": "/ezerbelhassen/inspirity-react/static/media/Roboto-Thin.dfe56a87.eot"
+  },
+  {
+    "revision": "a990f611f2305dc12965f186c2ef2690",
+    "url": "/ezerbelhassen/inspirity-react/static/media/Roboto-Light.a990f611.eot"
+  },
+  {
+    "revision": "30799efa5bf74129468ad4e257551dc3",
+    "url": "/ezerbelhassen/inspirity-react/static/media/Roboto-Regular.30799efa.eot"
+  },
+  {
+    "revision": "4d9f3f9e5195e7b074bb63ba4ce42208",
+    "url": "/ezerbelhassen/inspirity-react/static/media/Roboto-Medium.4d9f3f9e.eot"
+  },
+  {
+    "revision": "14e371796624f8bcbb41",
+    "url": "/ezerbelhassen/inspirity-react/static/js/main.14e37179.chunk.js"
+  },
+  {
+    "revision": "954bbdeb86483e4ffea00c4591530ece",
+    "url": "/ezerbelhassen/inspirity-react/static/media/Roboto-Thin.954bbdeb.woff2"
+  },
+  {
+    "revision": "69f8a0617ac472f78e45841323a3df9e",
+    "url": "/ezerbelhassen/inspirity-react/static/media/Roboto-Light.69f8a061.woff2"
+  },
+  {
+    "revision": "574fd0b50367f886d359e8264938fc37",
+    "url": "/ezerbelhassen/inspirity-react/static/media/Roboto-Medium.574fd0b5.woff2"
+  },
+  {
+    "revision": "39b2c3031be6b4ea96e2e3e95d307814",
+    "url": "/ezerbelhassen/inspirity-react/static/media/Roboto-Bold.39b2c303.woff2"
+  },
+  {
+    "revision": "2751ee43015f9884c3642f103b7f70c9",
+    "url": "/ezerbelhassen/inspirity-react/static/media/Roboto-Regular.2751ee43.woff2"
+  },
+  {
+    "revision": "7500519de3d82e33d1587f8042e2afcb",
+    "url": "/ezerbelhassen/inspirity-react/static/media/Roboto-Thin.7500519d.woff"
+  },
+  {
+    "revision": "3b813c2ae0d04909a33a18d792912ee7",
+    "url": "/ezerbelhassen/inspirity-react/static/media/Roboto-Light.3b813c2a.woff"
+  },
+  {
+    "revision": "ba3dcd8903e3d0af5de7792777f8ae0d",
+    "url": "/ezerbelhassen/inspirity-react/static/media/Roboto-Regular.ba3dcd89.woff"
+  },
+  {
+    "revision": "fc78759e93a6cac50458610e3d9d63a0",
+    "url": "/ezerbelhassen/inspirity-react/static/media/Roboto-Medium.fc78759e.woff"
+  },
+  {
+    "revision": "dc81817def276b4f21395f7ea5e88dcd",
+    "url": "/ezerbelhassen/inspirity-react/static/media/Roboto-Bold.dc81817d.woff"
+  },
+  {
+    "revision": "94998475f6aea65f558494802416c1cf",
+    "url": "/ezerbelhassen/inspirity-react/static/media/Roboto-Thin.94998475.ttf"
+  },
+  {
+    "revision": "df7b648ce5356ea1ebce435b3459fd60",
+    "url": "/ezerbelhassen/inspirity-react/static/media/Roboto-Regular.df7b648c.ttf"
+  },
+  {
+    "revision": "894a2ede85a483bf9bedefd4db45cdb9",
+    "url": "/ezerbelhassen/inspirity-react/static/media/Roboto-Medium.894a2ede.ttf"
+  },
+  {
+    "revision": "46e48ce0628835f68a7369d0254e4283",
+    "url": "/ezerbelhassen/inspirity-react/static/media/Roboto-Light.46e48ce0.ttf"
+  },
+  {
+    "revision": "330df886e97df17de249",
+    "url": "/ezerbelhassen/inspirity-react/static/css/1.faec656f.chunk.css"
+  },
+  {
+    "revision": "be68e5f2b222095ed6e94a825c07e31a",
+    "url": "/ezerbelhassen/inspirity-react/index.html"
+  }
+];
